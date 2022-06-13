847c223c14d95513435888af2bbf3fcab907a939
Andres Pozo Munoz <andres.munoz@suse.com>
2022-06-13 10:47:14 +0200

    adding gallery commands to cli

--- azure_img_utils/cli/cli.py
+++ azure_img_utils/cli/cli.py
@@ -24,6 +24,7 @@ import click
 
 from azure_img_utils.cli.blob import blob
 from azure_img_utils.cli.image import image
+from azure_img_utils.cli.gallery import gallery
 
 
 # -----------------------------------------------------------------------------
@@ -62,3 +63,4 @@ def az_img_utils(context):
 
 az_img_utils.add_command(blob)
 az_img_utils.add_command(image)
+az_img_utils.add_command(gallery)
